b5180b8e7551af2af5abc8da37a0ad06420ae4a5
saurav <sudhirsaurav17@gmail.com>
2024-01-19 18:48:39 +0530

    added button for skip

--- skip.js
+++ skip.js
@@ -1,2 +1,10 @@
-const currentUrl = window.location.href;
-console.log(currentUrl);
+// Check for skipButton only after the extension is opened
+window.addEventListener("DOMContentLoaded", (event) => {
+  document.getElementById("skipButton").addEventListener("click", myFunction);
+});
+
+function myFunction() {
+  const currentUrl = window.location.href;
+  console.log(currentUrl);
+  alert("Testing Button");
+}
